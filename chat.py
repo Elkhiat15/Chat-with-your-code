022e5148bc786560670e5ffbe88ec286721476df
Mohamed Elkhiat <mohammed.khayyat02@eng-st.cu.edu.eg>
2024-09-10 22:40:56 +0300

    Setup  commit

---
+++ chat.py
@@ -0,0 +1,3 @@
+from llama_index
+Link = "https://github.com/Elkhiat15/Large-Document-Summarization"
+# pip install llama-index-embeddings-gemini llama-index-llms-gemini llama-index-readers-github python-dotenv
